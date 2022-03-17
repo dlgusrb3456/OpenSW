5fd8b67e75057985bf5f87a96fc9b6a4c2f452df
dlgusrb3456 <dlgusrb3456@naver.com>
2022-03-17 12:45:33 +0900

    make index.xml

--- src/operate.java
+++ src/operate.java
@@ -1,6 +1,12 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
 import org.w3c.dom.Element;
+//import org.w3c.dom.Element;
+
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -21,8 +27,8 @@ public class operate {
     }
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException {
-        File[] files = getFile("C:\\Users\\dlgus\\OneDrive\\바탕 화면\\SimpleLR\\dkfj");
-
+        //File[] files = getFile("C:\\Users\\dlgus\\OneDrive\\바탕 화면\\SimpleLR\\src\\collection.xml");
+        File file = new File("C:\\Users\\dlgus\\OneDrive\\바탕 화면\\SimpleLR\\src\\collection.xml");
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
@@ -30,25 +36,44 @@ public class operate {
         Element docs = doc.createElement("docs");
         doc.appendChild(docs);
 
-        for(int i=0;i<files.length;i++){
-            Document fileDoc = Jsoup.parse(files[i], "UTF-8");
+        Document fileDoc = Jsoup.parse(file, "UTF-8");
+        Elements els = fileDoc.select("doc");
+        for(int i=0;i<els.size();i++){
+            System.out.println(els.get(i).select("title").text());
+            System.out.println(els.get(i).textNodes().get(0).text());
+
+
             Element docID = doc.createElement("doc");
-            docs.appendChild(docID);
             docID.setAttribute("id",Integer.toString(i));
+            docs.appendChild(docID);
             Element docTitle = doc.createElement("title");
             Element docBody = doc.createElement("body");
+
             docTitle.appendChild(doc.createTextNode(fileDoc.title()+'\n'));
-            docBody.appendChild(doc.createTextNode(fileDoc.body().text()+'\n'));
+
+            String setString = els.get(i).textNodes().get(0).text();
+            KeywordExtractor ke = new KeywordExtractor();
+            KeywordList kl = ke.extractKeyword(setString,true);
+            String str = "";
+            for(int k=0;k<kl.size();k++){
+                Keyword kwrd = kl.get(k);
+                str+=kwrd.getString()+':'+kwrd.getCnt()+'#';
+            }
+
+
+            docBody.appendChild(doc.createTextNode(str+'\n'));
             docID.appendChild(docTitle);
             docID.appendChild(docBody);
-        };
+        }
 
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.ENCODING,"UTF-8");
 
         DOMSource source = new DOMSource(doc);
-        StreamResult result = new StreamResult(new FileOutputStream(new File("src/book.xml")));
+        StreamResult result = new StreamResult(new FileOutputStream(new File("src/index.xml")));
         transformer.transform(source,result);
+
+
     }
 }
